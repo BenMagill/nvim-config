7d9a2c0131914a6a6ac33925544aaee9e64dfa9f
benmagill <benmagill@protonmail.com>
2023-11-09 16:48:06 +0000

    copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,50 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      build = ":Copilot auth",
+      opts = {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+        filetypes = {
+          markdown = true,
+          help = true,
+        },
+      },
+    },
+
+    --{
+    --"zbirenbaum/copilot.lua",
+    --cmd = "Copilot",
+    --build = ":Copilot auth",
+    --opts = {
+    --suggestion = { enabled = true, auto_trigger = true, accept = "<C-k>" },
+    --panel = { enabled = false, auto_refresh = true },
+    --filetypes = {
+    --markdown = true,
+    --help = true,
+    --},
+    --},
+    --},
+    --{
+    --"zbirenbaum/copilot-cmp",
+    --dependencies = "copilot.lua",
+    --opts = {},
+    --config = function(_, opts)
+    --local copilot_cmp = require("copilot_cmp")
+    --copilot_cmp.setup(opts)
+    ---- attach cmp source whenever copilot attaches
+    ---- fixes lazy-loading issues with the copilot cmp source
+    --require("lazyvim.util").lsp.on_attach(function(client)
+    --if client.name == "copilot" then
+    --copilot_cmp._on_insert_enter({})
+    --end
+    --end)
+    --end,
+    --},
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
